034751a25e3c83e335b75b67cb64b7e41e0ceb60
Ayoub Aitbelhaj <aaitbelh@e1r11p9.1337.ma>
2022-08-30 23:09:19 +0100

    finish day06 and add day07 ex02

--- cpp_module07/ex02/Array.cpp
+++ cpp_module07/ex02/Array.cpp
@@ -1,35 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Array.hpp                                          :+:      :+:    :+:   */
+/*   Array.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaitbelh <aaitbelh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/30 18:29:10 by aaitbelh          #+#    #+#             */
-/*   Updated: 2022/08/30 23:06:06 by aaitbelh         ###   ########.fr       */
+/*   Created: 2022/08/30 22:47:51 by aaitbelh          #+#    #+#             */
+/*   Updated: 2022/08/30 23:05:48 by aaitbelh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARRAY_HPP
-#define ARRAY_HPP
+#include "Array.hpp"
 
+Array::Array(){
+	Type = nullptr;
+}
 
-#include <string>
-#include <iostream>
-
-
-template<typename T> class Array
+Array::Array(std::size_t n)
 {
-	private:
-		T *Type;
-		std::size_t Size;
-	public:
-		Array();
-		Array(std::size_t n);
-		Array(Array &Other);
-		Array& operator=(Array &Other);
-		~Array();
-};
+	Type = new T[n];
+	this->_size = n;
+}
 
+Array::~Array()
+{
+	if(_size != 0)
+		delete [] Type;
+}
 
-#endif
+Array<int>;
