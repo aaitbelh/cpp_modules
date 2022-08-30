9433abcfc4ebe38ea5b556be9d623a865d1e765b
Ayoub Aitbelhaj <aaitbelh@e1r11p9.1337.ma>
2022-08-30 18:56:05 +0100

    finish day06 and add day07 ex02

--- cpp_module07/ex01/main.cpp
+++ cpp_module07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aaitbelh <aaitbelh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 21:38:28 by aaitbelh          #+#    #+#             */
-/*   Updated: 2022/08/30 11:24:57 by aaitbelh         ###   ########.fr       */
+/*   Updated: 2022/08/30 13:00:46 by aaitbelh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,29 @@
 #include <string>
 #include <iostream>
 
+template<typename T>
+template<typename N>
+void foo(T a, N b)
+{
+	std::cout << a << 
+}
+
+void _simpleprint(int &value)
+{
+	std::cout << value << std::endl;
+}
+
 template<typename T> void ADD1(T &value)
 {
 	value += 10;
 }
 
+
 int main()
 {
 	int array[5] = {1,2,3,4,5};
-	iter(&array[0],5, ADD1<int &>);
+
+	iter(&array[0],5, _simpleprint);
 	for(int i = 0; i < (int)(sizeof(array) / sizeof(array[0])); ++i)
 		std::cout << array[i] << std::endl;
 }
--- cpp_module07/ex02/Array.hpp
+++ cpp_module07/ex02/Array.hpp
@@ -1,40 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaitbelh <aaitbelh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/29 18:40:05 by aaitbelh          #+#    #+#             */
-/*   Updated: 2022/08/30 15:05:30 by aaitbelh         ###   ########.fr       */
+/*   Created: 2022/08/30 18:29:10 by aaitbelh          #+#    #+#             */
+/*   Updated: 2022/08/30 18:29:53 by aaitbelh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WHATEVER_HPP
-#define WHATEVER_HPP
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
 
-#include <iostream>
-#include <string>
 
-template<typename T> void swap(T &a, T &b)
-{
-	T c = a;
-	a = b;
-	b = c;
-}
-template<typename T> T min(T &a, T &b)
-{
-	if(a < b)
-		return (a);
-	else 
-		return (b);
-}
-template<typename T> T max(T &a, T &b)
-{
-	if(a > b)
-		return (a);
-	else 
-		return (b);
-}
+
 
 #endif
