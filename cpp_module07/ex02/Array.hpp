6e10164f1be33a473d098573cb8e58878b7adc63
Ayoub Aitbelhaj <aaitbelh@e1r10p4.1337.ma>
2022-08-31 18:13:34 +0100

    finish day08 and add day08 ex01

--- cpp_module07/ex02/Array.hpp
+++ cpp_module07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: aaitbelh <aaitbelh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 18:29:10 by aaitbelh          #+#    #+#             */
-/*   Updated: 2022/08/30 23:06:06 by aaitbelh         ###   ########.fr       */
+/*   Updated: 2022/08/31 13:53:16 by aaitbelh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,65 @@
 
 #include <string>
 #include <iostream>
+#include <exception>
 
 
 template<typename T> class Array
 {
 	private:
 		T *Type;
-		std::size_t Size;
+		std::size_t _Size;
 	public:
-		Array();
-		Array(std::size_t n);
-		Array(Array &Other);
-		Array& operator=(Array &Other);
-		~Array();
+		Array()
+		{
+			this->Type = NULL;
+			_Size = 0;
+		}
+		Array(std::size_t n)
+		{
+			Type = 	new T[n];
+			this->_Size = n;
+		}
+		T& getIndex(int index)
+		{
+			return (this->Type[index]);
+		}
+		T& operatori[](nt index)
+		{
+			if(index < 0)
+				throw(std::overflow_error("ERROR: OVERFLOW"));
+			else if(index >= this->size())
+				throw(std::domain_error("ERROR: UNDERFLOW"));
+			else
+				return (this->Type[index]);
+			return (*Type);
+		}
+		int size()
+		{
+			return this->_Size;
+		}
+		Array(Array &Other)
+		{
+			this->Type = new T[Other.size()];
+			this->_Size = Other.size();
+			for(int i = 0; i < Other.size(); ++i)
+			{
+				this->Type[i] = Other[i];
+			}
+		}
+		Array& operator=(Array &Other)
+		{
+			std::cout << "operator called here" << std::endl;
+			if(this->size())
+				delete [] this->Type;
+			for(int i = 0 ;i < Other.size(); ++i)
+				this->Type[i] = Other[i];
+			return (*this);
+		}
+		~Array(){
+			if(this->size())
+				delete [] this->Type;
+		};
 };
 
 
--- cpp_module07/ex02/main.cpp
+++ cpp_module07/ex02/main.cpp
@@ -6,13 +6,71 @@
 /*   By: aaitbelh <aaitbelh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 22:07:40 by aaitbelh          #+#    #+#             */
-/*   Updated: 2022/08/30 22:07:58 by aaitbelh         ###   ########.fr       */
+/*   Updated: 2022/08/31 11:01:31 by aaitbelh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "Array.hpp"
+
+#define MAX_VAL 750
+int foo()
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = 1 + rand() % 100;
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		std::cout << numbers.size() << std::endl;
+		std::cout << tmp.size() << std::endl;
+		Array<int> test(tmp);
+		for(int i = 0 ; i < 750; ++i)
+		{
+			std::cout << tmp[i] << " ";
+		}
+	}
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+		try
+		{
+			numbers[-2] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		try
+		{
+			numbers[MAX_VAL] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		delete [] mirror;//
+		return (0);
+}
+
 int main()
 {
-	int *a = new int();
-	std::cout << *a << std::endl;
+	foo();
+	system("leaks a.out");
 }
